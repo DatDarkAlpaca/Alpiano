955c0e622c1aca0e307560fc7503b06dd6b68b05
Alpaca <70183390+DatDarkAlpaca@users.noreply.github.com>
2022-02-21 07:31:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,170 @@
+// Audio Context:
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+let ctx;
+
+// Oscilators:
+const oscilators = {};
+
+// Context Start Button:
+const ctxStartButton = document.querySelector('button');
+ctxStartButton.addEventListener('click', () => {
+    ctx = new AudioContext();
+});
+
+// Convert MIDI to Frequency:
+function midiToFrequency(note) {
+    const A = 440;
+    return (A / 32) * (2 ** ((note - 9) / 12));
+}
+
+function midiToKey(note) {
+    const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+    noteSymbol = notes[note % 12];
+    noteOctave = Math.floor(note / 12) - 1;
+
+    return `${noteSymbol}${noteOctave}`;
+}
+
+function symbolToFrequency(note) {
+    const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+    let noteSymbol;
+    if(note.length === 2) {
+        noteSymbol = note[0];
+    } else {
+        noteSymbol = note[0] + note[1];
+    }
+
+    const noteNumber = notes.indexOf(noteSymbol);
+    const noteOctave = parseInt(note[note.length - 1]) + 1;
+
+    return midiToFrequency(noteOctave * 12 + noteNumber);
+}
+
+// Checking the MIDI device.
+if(navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess().then(success, failure);
+}
+
+// Request Callbacks:
+function success(midiAccess) {
+    midiAccess.addEventListener('statechange', updateDevices);
+
+    const inputs = midiAccess.inputs;
+
+    // On Keyboard press:
+    inputs.forEach((input) => {
+        input.addEventListener('midimessage', handleInput);
+    });
+}
+
+function failure() {
+    console.log('Could not connect to the MIDI device.');
+}
+
+// Update Callback:
+function updateDevices(event) {
+    console.log(`Name: ${event.port.name} | State: ${event.port.state}, Type: ${event.port.type}`);
+}
+
+// Handle:
+function handleInput(input) {
+    const command  = input.data[0];
+    const note     = input.data[1];
+    const velocity = input.data[2];
+
+    switch(command)
+    {
+        case 144:
+            if(velocity > 0) {
+                onNotePressed(note, velocity);
+            }
+            else {
+                onNoteReleased(note); // Off.
+            }
+            break;
+
+        case 128:
+            onNoteReleased(note); // Released.
+            break;
+    }
+}
+
+// Key Events:
+function playNoteSymbol(note) {
+    if(!ctx) {
+        return;
+    }
+
+    // Sound:
+    const osc = ctx.createOscillator();
+    osc.type = 'sine';
+    osc.frequency.value = symbolToFrequency(note);
+
+    const oscGain = ctx.createGain();
+    oscGain.gain.value = 0.33; // general volume
+
+    osc.connect(oscGain);
+    oscGain.connect(ctx.destination);
+
+    osc.gain = oscGain;
+
+    oscilators[note.toString()] = osc;
+
+    osc.start();
+}
+
+function onNotePressed(note, velocity) {
+    if(!ctx) {
+        return;
+    }
+
+    // Sound:
+    const osc = ctx.createOscillator();
+
+    const oscGain = ctx.createGain();
+    oscGain.gain.value = 0.33; // general volume
+
+    const convertedGain = (1 / 127) * velocity;
+    const velocityGain = ctx.createGain();
+    velocityGain.gain.value = convertedGain;
+
+    osc.type = 'sine';
+    osc.frequency.value = midiToFrequency(note);
+
+    osc.connect(oscGain);
+    oscGain.connect(velocityGain);
+    velocityGain.connect(ctx.destination);
+
+    osc.gain = oscGain;
+
+    oscilators[note.toString()] = osc;
+
+    osc.start();
+
+    // Display:
+    app.displayNote(midiToKey(note));
+}
+
+function onNoteReleased(note) {
+    const osc = oscilators[note.toString()];
+
+    if(!ctx || !osc) {
+        return;
+    }
+
+    const oscGain = osc.gain;
+
+    oscGain.gain.setValueAtTime(oscGain.gain.value, ctx.currentTime);
+    oscGain.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + 0.03);
+
+    setTimeout(() => {
+        osc.stop();
+        osc.disconnect();
+    }, 50);
+
+    app.hideNote(midiToKey(note));
+    
+    delete oscilators[note.toString()];
+}
---
+++ keyboard.js
@@ -0,0 +1,266 @@
+const naturalKeys = ["C", "D", "E", "F", "G", "A", "B"];
+const sharpKeys   = ["C", "D", "F", "G", "A"];
+const flatKeys    = ["D", "E", "G", "A", "B"];
+
+const range = ["G2", "D6"];
+
+const whiteKeyWidth = 80;
+const keyboardHeight = 400;
+
+const app = {
+    setupKeyboard() {
+        const keyboard = document.querySelector("#keyboard");
+
+        const allNaturalNotes = this.getAllNaturalNotes(range);
+        const keyboardWidth = allNaturalNotes.length * whiteKeyWidth;
+
+        // Main SVG:
+        const SVG = this.createMainSVG(keyboardWidth, keyboardHeight);
+        
+
+        // White keys:
+        let whiteKeyX = 0;
+        allNaturalNotes.forEach((noteName) => {
+            const whiteKeyTextGroup = utils.createSVGElement("g");
+            const whiteKey = this.createKey({ className: "white-key", width: whiteKeyWidth, height: keyboardHeight, noteName: noteName });
+            const text = utils.createSVGElement("text");
+            
+            utils.addTextContent(text, noteName);
+
+            utils.setAttributes(whiteKeyTextGroup, { "width": whiteKeyWidth });
+            utils.setAttributes(text, { 
+                "x": whiteKeyX + whiteKeyWidth / 2,
+                "y": 380,
+                "text-anchor": "middle" 
+            });
+            utils.setAttributes(whiteKey, {
+                "x": whiteKeyX,
+                "data-note-name": noteName,
+                "rx": "15",
+                "ry": "15"
+            });
+
+            text.classList.add("white-key-text");
+
+            whiteKeyTextGroup.appendChild(whiteKey);
+            whiteKeyTextGroup.appendChild(text);
+            SVG.appendChild(whiteKeyTextGroup);
+
+            whiteKeyX += whiteKeyWidth;
+        });
+
+        // Black keys:
+        let blackKeyX = 60;
+        allNaturalNotes.forEach((naturalNote, index, array) => {
+            if (index === array.length - 1) {
+                return;
+            }
+
+            const blackKeyTextGroup = utils.createSVGElement("g");
+            const flatNameText = utils.createSVGElement("text");
+            const sharpNameText = utils.createSVGElement("text");
+
+            const blackKey = this.createKey({className: "black-key", width: whiteKeyWidth / 2, height: keyboardHeight / 1.6, noteName: `${naturalNote[0]}#${naturalNote[1]}`});
+           
+            utils.setAttributes(blackKeyTextGroup, { "width": whiteKeyWidth / 2});
+
+            for(let i = 0; i < sharpKeys.length; ++i) {
+                let sharpName = sharpKeys[i];
+                let flatName  = flatKeys[i];
+
+                if(sharpName === naturalNote[0]) {
+                    utils.setAttributes(blackKey, {
+                        "x": blackKeyX,
+                        "data-sharp-name": `${sharpName}#${naturalNote[1]}`,
+                        "data-flat-name": `${flatName}b${naturalNote[1]}`,
+                        "rx": "8",
+                        "ry": "8"
+                    });
+
+                    utils.setAttributes(sharpNameText, {
+                        "text-anchor": "middle",
+                        "y": 215,
+                        "x": blackKeyX + (whiteKeyWidth / 4)
+                    });
+
+                    utils.setAttributes(flatNameText, {
+                        "text-anchor": "middle",
+                        "y": 235,
+                        "x": blackKeyX + (whiteKeyWidth / 4)
+                    });
+
+                    utils.addTextContent(sharpNameText, `${sharpName}♯`);
+                    utils.addTextContent(flatNameText, `${flatName}♭`);
+
+                    sharpNameText.classList.add("black-key-text");
+                    flatNameText.classList.add("black-key-text");
+                                        
+                    if(sharpName === "D" || sharpName === "A") {
+                        blackKeyX += whiteKeyWidth * 2;
+                    } else {
+                        blackKeyX += whiteKeyWidth;
+                    }
+
+                    blackKeyTextGroup.appendChild(blackKey);
+                    blackKeyTextGroup.appendChild(sharpNameText);
+                    blackKeyTextGroup.appendChild(flatNameText);
+                }
+            }
+
+            SVG.appendChild(blackKeyTextGroup);
+        });
+
+        keyboard.appendChild(SVG);
+    },
+
+    createOctave(i) {
+        const octave = utils.createSVGElement("g");
+        octave.classList.add("octave");
+        octave.setAttribute("transform", `translate(${ i * octaveWidth }, 0)`);
+        return octave;
+    },
+
+    createKey({ className, width, height, noteName }) {
+        const key = utils.createSVGElement("rect");
+        key.classList.add(className, "key");
+    
+        key.addEventListener('mousedown', () => {
+            this.displayNote(noteName);
+            playNoteSymbol(noteName);
+        });
+
+        key.addEventListener('mouseup', () => {
+            this.hideNote(noteName);
+            onNoteReleased(noteName);
+        });
+
+        key.addEventListener('mouseleave', () => {
+            this.hideNote(noteName);
+            onNoteReleased(noteName);
+        });
+
+        utils.setAttributes(key, {
+            "width": width,
+            "height": height
+        });
+
+        return key;
+    },
+
+    getAllNaturalNotes([firstKey, lastKey]) {
+        const firstKeyName = firstKey[0];
+        const firstKeyNumber = parseInt(firstKey[1]);
+
+        const lastKeyName = lastKey[0];
+        const lastKeyNumber = parseInt(lastKey[1]);
+
+        const firstKeyIndex = naturalKeys.indexOf(firstKeyName);
+        const lastKeyIndex = naturalKeys.indexOf(lastKeyName);
+
+        const allNaturalKeys = [];
+
+        for(let octaveNumber = firstKeyNumber; octaveNumber < lastKeyNumber; ++octaveNumber) {
+            if(octaveNumber === firstKeyNumber) {
+                naturalKeys.slice(firstKeyIndex).forEach((noteName) => {
+                    allNaturalKeys.push(noteName + octaveNumber);
+                });
+            } else if(octaveNumber === lastKeyNumber) {
+                naturalKeys.slice(0, lastKeyIndex + 1).forEach((noteName) => {
+                    allNaturalKeys.push(noteName + octaveNumber);
+                });
+            } else {
+                naturalKeys.forEach((noteName) => {
+                    allNaturalKeys.push(noteName + octaveNumber);
+                });
+            }
+        }
+
+        return allNaturalKeys;
+    },
+
+    createMainSVG(keyboardWidth, keyboardHeight) {
+        const SVG = utils.createSVGElement("svg");
+
+        utils.setAttributes(SVG, {
+            "width": "100%",
+            "version": "1.1",
+            "xmlns": "http://www.w3.org/2000/svg",
+            "xmlns:xlink": "http://www.w3.org/1999/xlink",
+            "viewBox": `0 0 ${keyboardWidth} ${keyboardHeight}`
+        });
+
+        return SVG;
+    },
+
+    displayNote(note){
+        const keyboardKeys = document.querySelectorAll(".key");
+
+        keyboardKeys.forEach(key => {
+            const naturalName = key.dataset.noteName;
+            const sharpName = key.dataset.sharpName;
+            const flatName = key.dataset.flatName;
+
+            if(naturalName === note || sharpName === note || flatName === note) {
+                key.classList.add("show");
+            }
+        });
+    },
+
+    hideNote(note) {
+        const keyboardKeys = document.querySelectorAll(".key");
+
+        keyboardKeys.forEach(key => {
+            const naturalName = key.dataset.noteName;
+            const sharpName = key.dataset.sharpName;
+            const flatName = key.dataset.flatName;
+
+            if(naturalName === note || sharpName === note || flatName === note) {
+                key.classList.remove("show");
+            }
+        });
+    },
+
+    displayNotes(notes) {
+        const keyboardKeys = document.querySelectorAll(".key");
+        utils.removeClassFromNodeCollection(keyboardKeys, "show");
+
+        notes.forEach(noteName => {
+            keyboardKeys.forEach(key => {
+                const naturalName = key.dataset.noteName;
+                const sharpName = key.dataset.sharpName;
+                const flatName = key.dataset.flatName;
+
+                if(naturalName === noteName || sharpName === noteName || flatName === noteName) {
+                    key.classList.add("show");
+                }
+            });
+        });
+    }
+}
+
+const utils = {
+    createSVGElement(el) {
+        const element = document.createElementNS("http://www.w3.org/2000/svg" , el);
+        return element;
+    },
+
+    setAttributes(el, attrs) {
+        for(let key in attrs) {
+            el.setAttribute(key, attrs[key]);
+        }
+    },
+
+    addTextContent(el, content) {
+        el.textContent = content;
+    },
+
+    removeClassFromNodeCollection(nodeCollection, classToRemove) {
+        nodeCollection.forEach(node => {
+           if (node.classList.contains(classToRemove)) {
+               node.classList.remove(classToRemove);
+           }
+        });
+    }
+}
+
+app.setupKeyboard();
