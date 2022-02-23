7b1fe8e98396e17962c35e48a9b59be396e87e34
Alpaca <70183390+DatDarkAlpaca@users.noreply.github.com>
2022-02-23 02:52:18 -0400

    Improve keyboard and add range control

--- keyboard.js
+++ keyboard.js
@@ -2,7 +2,7 @@ const naturalKeys = ["C", "D", "E", "F", "G", "A", "B"];
 const sharpKeys   = ["C", "D", "F", "G", "A"];
 const flatKeys    = ["D", "E", "G", "A", "B"];
 
-const range = ["G2", "D6"];
+const range = ["C3", "C5"];
 
 const whiteKeyWidth = 80;
 const keyboardHeight = 400;
@@ -22,7 +22,6 @@ const app = {
         // Main SVG:
         const SVG = this.createMainSVG(keyboardWidth, keyboardHeight);
         
-
         // White keys:
         let whiteKeyX = 0;
         allNaturalNotes.forEach((noteName) => {
@@ -118,6 +117,207 @@ const app = {
         keyboard.appendChild(SVG);
     },
 
+    editKeyboard(){
+        const allNaturalNotes = this.getAllNaturalNotes(range);
+
+        // Change SVG:
+        const SVG = document.querySelector(".keyboard-SVG");
+        const keyboardWidth = allNaturalNotes.length * whiteKeyWidth;
+        SVG.setAttribute("viewBox", `0 0 ${keyboardWidth} ${keyboardHeight}`)
+
+        // White keys:
+        let whiteKeyX = 0;
+        allNaturalNotes.forEach((noteName) => {
+            const whiteKeyTextGroup = utils.createSVGElement("g");
+            const whiteKey = this.createKey({ className: "white-key", width: whiteKeyWidth, height: keyboardHeight, noteName: noteName });
+            const text = utils.createSVGElement("text");
+            
+            utils.addTextContent(text, noteName);
+
+            utils.setAttributes(whiteKeyTextGroup, { "width": whiteKeyWidth });
+            utils.setAttributes(text, { 
+                "x": whiteKeyX + whiteKeyWidth / 2,
+                "y": 380,
+                "text-anchor": "middle" 
+            });
+            utils.setAttributes(whiteKey, {
+                "x": whiteKeyX,
+                "data-note-name": noteName,
+                "rx": "15",
+                "ry": "15"
+            });
+
+            text.classList.add("white-key-text");
+
+            whiteKeyTextGroup.appendChild(whiteKey);
+            whiteKeyTextGroup.appendChild(text);
+            SVG.appendChild(whiteKeyTextGroup);
+
+            whiteKeyX += whiteKeyWidth;
+        });
+
+        // Black keys:
+        let blackKeyX = 60;
+        allNaturalNotes.forEach((naturalNote, index, array) => {
+            if (index === array.length - 1) {
+                return;
+            }
+
+            const blackKeyTextGroup = utils.createSVGElement("g");
+            const flatNameText = utils.createSVGElement("text");
+            const sharpNameText = utils.createSVGElement("text");
+
+            const blackKey = this.createKey({className: "black-key", width: whiteKeyWidth / 2, height: keyboardHeight / 1.6, noteName: `${naturalNote[0]}#${naturalNote[1]}`});
+           
+            utils.setAttributes(blackKeyTextGroup, { "width": whiteKeyWidth / 2});
+
+            for(let i = 0; i < sharpKeys.length; ++i) {
+                let sharpName = sharpKeys[i];
+                let flatName  = flatKeys[i];
+
+                if(sharpName === naturalNote[0]) {
+                    utils.setAttributes(blackKey, {
+                        "x": blackKeyX,
+                        "data-sharp-name": `${sharpName}#${naturalNote[1]}`,
+                        "data-flat-name": `${flatName}b${naturalNote[1]}`,
+                        "rx": "8",
+                        "ry": "8"
+                    });
+
+                    utils.setAttributes(sharpNameText, {
+                        "text-anchor": "middle",
+                        "y": 215,
+                        "x": blackKeyX + (whiteKeyWidth / 4)
+                    });
+
+                    utils.setAttributes(flatNameText, {
+                        "text-anchor": "middle",
+                        "y": 235,
+                        "x": blackKeyX + (whiteKeyWidth / 4)
+                    });
+
+                    utils.addTextContent(sharpNameText, `${sharpName}♯`);
+                    utils.addTextContent(flatNameText, `${flatName}♭`);
+
+                    sharpNameText.classList.add("black-key-text");
+                    flatNameText.classList.add("black-key-text");
+                                        
+                    if(sharpName === "D" || sharpName === "A") {
+                        blackKeyX += whiteKeyWidth * 2;
+                    } else {
+                        blackKeyX += whiteKeyWidth;
+                    }
+
+                    blackKeyTextGroup.appendChild(blackKey);
+                    blackKeyTextGroup.appendChild(sharpNameText);
+                    blackKeyTextGroup.appendChild(flatNameText);
+                }
+            }
+
+            SVG.appendChild(blackKeyTextGroup);
+        });
+    },
+
+    /*
+    *   Todo: limiting, factor;
+    */
+    setupMenuKey() {
+        const keys = document.querySelectorAll(".menu-key-btn");
+        console.log(keys);
+
+        keys[0].addEventListener('click', () => {   // End +
+            const key = range[0];
+
+            if (key.length === 2) {
+                const keyLetter = range[1][0];
+                const octave = range[1][1];
+                console.log(keyLetter, octave);
+
+                const keyPos = naturalKeys.indexOf(keyLetter);
+
+                if (keyLetter !== 'B') {
+                    range[1] = naturalKeys[keyPos + 1] + octave;
+                    this.editKeyboard();
+                } else {
+                    range[1] = naturalKeys[0] + (parseInt(octave) + 1).toString();
+                    this.editKeyboard();
+                }
+
+                const menuBtn = document.querySelector("#key-menu-end");
+                menuBtn.innerHTML = range[1];
+            }            
+        });
+
+        keys[1].addEventListener('click', () => {   // End +
+            const key = range[0];
+
+            if (key.length === 2) {
+                const keyLetter = range[1][0];
+                const octave = range[1][1];
+                console.log(keyLetter, octave);
+
+                const keyPos = naturalKeys.indexOf(keyLetter);
+
+                if (keyLetter !== 'C') {
+                    range[1] = naturalKeys[keyPos - 1] + octave;
+                    this.editKeyboard();
+                } else {
+                    range[1] = naturalKeys[6] + (parseInt(octave) - 1).toString();
+                    this.editKeyboard();
+                }
+
+                const menuBtn = document.querySelector("#key-menu-end");
+                menuBtn.innerHTML = range[1];
+            }            
+        });
+
+        keys[2].addEventListener('click', () => {   // Start +
+            const key = range[0];
+
+            if (key.length === 2) {
+                const keyLetter = range[0][0];
+                const octave = range[0][1];
+                console.log(keyLetter, octave);
+
+                const keyPos = naturalKeys.indexOf(keyLetter);
+
+                if (keyLetter !== 'B') {
+                    range[0] = naturalKeys[keyPos + 1] + octave;
+                    this.editKeyboard();
+                } else {
+                    range[0] = naturalKeys[0] + (parseInt(octave) + 1).toString();
+                    this.editKeyboard();
+                }
+
+                const menuBtn = document.querySelector("#key-menu-start");
+                menuBtn.innerHTML = range[0];
+            }            
+        });
+
+        keys[3].addEventListener('click', () => {   // Start -
+            const key = range[0];
+
+            if (key.length === 2) {
+                const keyLetter = range[0][0];
+                const octave = range[0][1];
+                console.log(keyLetter, octave);
+
+                const keyPos = naturalKeys.indexOf(keyLetter);
+
+                if (keyLetter !== 'C') {
+                    range[0] = naturalKeys[keyPos - 1] + octave;
+                    this.editKeyboard();
+                } else {
+                    range[0] = naturalKeys[6] + (parseInt(octave) - 1).toString();
+                    this.editKeyboard();
+                }
+
+                const menuBtn = document.querySelector("#key-menu-start");
+                menuBtn.innerHTML = range[0];
+            }            
+        })
+    },
+
     createOctave(i) {
         const octave = utils.createSVGElement("g");
         octave.classList.add("octave");
@@ -173,7 +373,7 @@ const app = {
 
         const allNaturalKeys = [];
 
-        for(let octaveNumber = firstKeyNumber; octaveNumber < lastKeyNumber; ++octaveNumber) {
+        for(let octaveNumber = firstKeyNumber; octaveNumber <= lastKeyNumber; ++octaveNumber) {
             if(octaveNumber === firstKeyNumber) {
                 naturalKeys.slice(firstKeyIndex).forEach((noteName) => {
                     allNaturalKeys.push(noteName + octaveNumber);
@@ -196,6 +396,7 @@ const app = {
         const SVG = utils.createSVGElement("svg");
 
         utils.setAttributes(SVG, {
+            "class": "keyboard-SVG",
             "width": "100%",
             "version": "1.1",
             "xmlns": "http://www.w3.org/2000/svg",
@@ -209,7 +410,7 @@ const app = {
     displayNote(note){
         const keyboardKeys = document.querySelectorAll(".key");
 
-        const displayText = document.querySelector(".chord-display");
+        const displayText = document.querySelector(".menu-display");
         console.log(displayText);
         displayText.innerHTML = note;
 
@@ -282,3 +483,4 @@ const utils = {
 }
 
 app.setupKeyboard();
+app.setupMenuKey();
